4dbe6caf8103406100aac945fd33e8afdb621a48
Anand Jha <anandjha7424@gmail.com>
2023-09-25 16:45:09 +0530

    Filter added for top rated restaurents

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,31 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurant] = useState(resList);
   return (
     <div className="body">
       <div className="search-container">
         <h3>Search</h3>
       </div>
 
+      <div className="filter-res">
+        <button
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (restaurant) => restaurant.info.avgRating > 4
+            );
+            console.log(filteredList);
+            setListOfRestaurant(filteredList);
+          }}
+        >
+          Popular Restaurant
+        </button>
+      </div>
+
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
