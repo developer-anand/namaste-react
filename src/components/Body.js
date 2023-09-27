d7939a125a1310a4cf0c2562238231175799e016
Anand Jha <anandjha7424@gmail.com>
2023-09-27 14:25:00 +0530

    API Added mock data removed

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,43 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurant] = useState(resList);
-  return (
+  /* array Destrucing */
+  // const arr = useState(resList);
+  // const [listOfRestaurants, setListOfRestaurant] = arr;
+  // const listOfRestaurants = arr[0];
+  // const setListOfRestaurant = arr[1];
+  const [listOfRestaurants, setListOfRestaurant] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.9719951&lng=75.74914129999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+
+    console.log(json);
+    const arrayOfCards = json.data.cards;
+    const restaurant_list = "restaurant_grid_listing";
+
+    for (const cardObj of arrayOfCards) {
+      if (cardObj.card.card && cardObj.card.card.id === restaurant_list) {
+        const resData =
+          cardObj.card?.card?.gridElements?.infoWithStyle?.restaurants;
+        setListOfRestaurant(resData);
+      }
+    }
+    // setListOfRestaurant(
+    //   json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    // );
+  };
+
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="search-container">
         <h3>Search</h3>
